90d0ddbae8be7a07bfb7e2d02269a8e5f3ae7cbd
himself65 <761282619@qq.com>
2018-05-24 23:40:42 +0800

    主界面搭建完成搭建完成

--- src/router/Home.vue
+++ src/router/Home.vue
@@ -29,25 +29,42 @@
       </v-list>
     </v-navigation-drawer>
     <!-- ToolBar部分 -->
-    <v-toolbar color="blue lighten-2" dark fixed app>
+    <v-toolbar color="blue" dark fixed app>
       <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
       <v-toolbar-title>VueWeb</v-toolbar-title>
     </v-toolbar>
     <v-content>
-      <v-container fluid fill-height>
-        <v-layout
-          justify-center
-          align-center
-        >
-          <!--<el-button :plain="true" @click="alert('GG')">success</el-button>-->
-          <div v-for="article in articles" :key="article.id">
-
-          </div>
+      <v-container fluid
+                   style="min-height: 0;"
+                   grid-list-lg>
+        <v-layout row wrap>
+          <v-flex v-for="article in articles" v-bind:key="article.id" xs12>
+            <v-card class="" round>
+              <v-card-media class="white--text" v-bind:src="article.head_img_url" height="200px">
+                <v-container fill-height fluid>
+                  <v-layout fill-height>
+                    <v-flex xs12 align-end flexbox>
+                      <span class="headline">{{ article.title }}</span>
+                    </v-flex>
+                  </v-layout>
+                </v-container>
+              </v-card-media>
+              <v-card-text primary-title>
+                {{ article.content }}
+              </v-card-text>
+              <v-card-actions xs12 sm5 md3>
+                <v-btn @click.stop="" color="orange" flat>查看</v-btn>
+                <v-btn icon>
+                  <v-icon>thumb_up</v-icon>
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-flex>
         </v-layout>
       </v-container>
     </v-content>
     <!-- Footer部分 -->
-    <v-footer color="blue lighten-2" app inset>
+    <v-footer color="blue" app inset>
       <span class="white--text">&copy; 2018 Code by Himself65</span>
     </v-footer>
   </v-app>
@@ -62,8 +79,6 @@ import api from '../utils/api'
 export default {
   name: 'Home',
   data: () => {
-    api.getAllArticles().then(res => { this.articles = res }
-    )
     return ({
       articles: [],
       drawer: false
@@ -72,16 +87,29 @@ export default {
   methods: {
     open_url: function (url) {
       if (url === undefined) {
-        // TODO
+        // @TODO
         // 提醒用户您啥也没写
       } else {
-        // TODO
+        // @TODO
         // 跳转链接
       }
     },
     alert: function (msg) {
-
+      // @TODO
+      // 后期改到全局方法
+    },
+    get_list () {
+      let _this = this
+      api.getAllArticles().then(function (data) {
+        _this.articles = data.data
+      })
     }
+  },
+  mounted: function () {
+    let _this = this
+    this.$nextTick(function () {
+      _this.get_list()
+    })
   }
 }
 </script>
